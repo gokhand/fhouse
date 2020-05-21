487f73356c5b22cdedc5a06c4d81954248785882
Gokhan Darcan <gokhan@darcan.net>
2020-05-21 20:54:32 +0100

    Routing configuration added

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('report', 'FHouseController@report');
+Route::get('transactions', 'FHouseController@transactions');
+Route::get('transaction/{transactionId}', 'FHouseController@singleTransaction');
+Route::get('show/{id}', 'FHouseController@show');
+
+Route::get('/main', 'MainController@index');
+Route::post('/main/checklogin', 'MainController@checklogin');
+Route::get('/main/logout', 'MainController@logout');
